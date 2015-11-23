96772170f2c08b2b4e0139f2c2611310a9b00ac2
Michal Szczepanski <michal@vane.pl>
2015-11-23 19:51:13 +0100

    move dbconnection url rules to dbconnection.py

--- web/router.py
+++ web/router.py
@@ -23,15 +23,4 @@ class RulesRouter:
         self.app.add_url_rule(self.prefix+'/db/test', 'db_test', self.data.db_test, methods=['POST'])
         self.app.add_url_rule(self.prefix+'/code/generate', 'code_generate', self.data.code_generate, methods=['POST'])
         # dbconnection
-        self.app.add_url_rule(self.prefix+'/db/connection/add', 'db_connection_add',
-                              self.dbconnection.add_connection, methods=['POST'])
-        self.app.add_url_rule(self.prefix+'/db/connection/remove', 'db_connection_remove',
-                              self.dbconnection.remove_connection, methods=['POST'])
-        self.app.add_url_rule(self.prefix+'/db/connection/list', 'db_connection_list',
-                              self.dbconnection.list_connection, methods=['GET'])
-        self.app.add_url_rule(self.prefix+'/db/connection/test', 'db_connection_test',
-                              self.dbconnection.test_connection, methods=['POST'])
-        self.app.add_url_rule(self.prefix+'/db/query', 'db_query',
-                              self.dbconnection.query, methods=['POST'])
-        self.app.add_url_rule(self.prefix+'/db/structure', 'db_structure',
-                              self.dbconnection.db_structure, methods=['GET'])
+        self.dbconnection.configure(self.prefix)
--- web/view/dbconnection.py
+++ web/view/dbconnection.py
@@ -14,8 +14,19 @@ class DBConnectionRouter:
         self.db = db
         self.cache = cache
 
+    def configure(self, prefix):
+        prefix += '/db'
+        self.app.add_url_rule(prefix+'/connection/add', 'db_connection_add', self.add_connection, methods=['POST'])
+        self.app.add_url_rule(prefix+'/connection/remove', 'db_connection_remove',
+                              self.remove_connection, methods=['POST'])
+        self.app.add_url_rule(prefix+'/connection/list', 'db_connection_list', self.list_connection, methods=['GET'])
+        self.app.add_url_rule(prefix+'/connection/test', 'db_connection_test', self.test_connection, methods=['POST'])
+        self.app.add_url_rule(prefix+'/query', 'db_query', self.query, methods=['POST'])
+        self.app.add_url_rule(prefix+'/structure', 'db_structure', self.db_structure, methods=['GET'])
+
     def add_connection(self):
         data = json.loads(request.data)
+        self.cache.conne
         return json.dumps({'code': -1, 'msg': 'TODO', 'time': 10000, 'data': None})
 
     def remove_connection(self):
