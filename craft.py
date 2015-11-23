8f996da7b8a341d8403c027d4e6c47f8e4dbc644
Michal Szczepanski <michal@vane.pl>
2015-11-23 19:37:50 +0100

    logging directory loaded from www.json / create logging directory if not exist

--- craft.py
+++ craft.py
@@ -2,17 +2,31 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Michal Szczepanski'
 
+import os
 import logging
 import logging.config
 from web import server
 from flask import json
 
 
-def start():
+def init_logging(directory):
+    # check log directory exists
+    if not os.path.exists(directory):
+        try:
+            os.makedirs(directory)
+        except:
+            raise RuntimeError('Cannot create directory for logs : %s' % directory)
     # load logging configuration
-    logging.config.fileConfig('conf/logging.ini')
-    # load server configuration
+    logging.config.fileConfig('conf/logging.ini', defaults={
+        'logdirectory': directory
+    })
+
+
+def start():
     conf = json.load(open('conf/www.json'))
+    init_logging(conf['logging']['directory'])
+    # load server configuration
+
     www = conf['www']
     db_path = conf['database']
     # start web server
